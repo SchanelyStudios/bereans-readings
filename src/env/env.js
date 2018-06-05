c84b360b4d8592458a5eb15ad6ae608fd7c3c37c
Phil Schanely <philschanely@gmail.com>
2018-06-05 07:32:34 -0400

    Add environment files

---
+++ src/env/env.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "api-domain",
+  databaseURL: "firebase-url",
+  projectId: "project-id",
+  storageBucket: "",
+  messagingSenderId: "000000000000"
+};
+export default config;
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,13 +1,6 @@
 import * as firebase from 'firebase';
+import config from '../env/env'
 
-const config = {
-  apiKey: "AIzaSyBD67TU74DKZdG7PNm89O5xLtIVbdrCK10",
-  authDomain: "bereans-readings.firebaseapp.com",
-  databaseURL: "https://bereans-readings.firebaseio.com",
-  projectId: "bereans-readings",
-  storageBucket: "",
-  messagingSenderId: "768518202170"
-};
 firebase.initializeApp(config);
 
 export default firebase;
