6aef9a9f8b6c039b0f14f201fa7d79f498019138
Phil Schanely <philschanely@gmail.com>
2018-06-07 07:43:39 -0400

    Add new user, replace updated user

--- src/services/auth.js
+++ src/services/auth.js
@@ -6,27 +6,35 @@ export default class AuthService {
     let userId = window.localStorage.getItem('BRR_USER_UID');
     let authed = window.localStorage.getItem('BRR_USER_AUTH');
     let access = window.localStorage.getItem('BRR_USER_ACCESS_LEVEL');
-    console.log('auth info', userId, authed, access);
     if (userId !== 'null' && authed && access >= 0) {
-      console.log('access granted');
       return true;
     }
-    console.log('access denied');
     return false;
   }
 
   static addAuthenticated(user) {
-    console.log('adding authed user', user);
     window.localStorage.getItem('BRR_USER_UID', user.uid);
     window.localStorage.getItem('BRR_USER_AUTH', true);
-    window.localStorage.getItem('BRR_USER_ACCESS_LEVEL', 0);
+    window.localStorage.getItem('BRR_USER_ACCESS_LEVEL', user.access_level);
+    return user.access_level > 0 ? true : false;
   }
 
   static addUnauthed(user) {
-    console.log('add unauthed/unverified user', user);
     window.localStorage.getItem('BRR_USER_UID', user.uid);
     window.localStorage.getItem('BRR_USER_AUTH', false);
     window.localStorage.getItem('BRR_USER_ACCESS_LEVEL', 0);
+    this.addUser({ uid: user.uid, email: user.email, access_level: 0 });
+  }
+
+  static addUser(data) {
+    return firebase.database().ref('users/' + data.uid).set({
+      email: data.email,
+      access_level: data.access_level
+    }).then(() => {
+      return data;
+    }).catch((error) => {
+      console.error('Failed to save user', error);
+    });
   }
 
   static checkAuth(user) {
@@ -35,29 +43,61 @@ export default class AuthService {
     }
 
     if (!user || !user.uid) {
-      console.log('no valid user', user);
       return false;
     }
 
-    return this.getUser(user.uid).then(res => {
-      console.log('checkauth result', res);
+    return this.getUser(null, user.email).then(res => {
+      let authorized = false;
       if (res) {
-        this.addAuthenticated(user);
+        if (res.uid !== user.uid) {
+          authorized = this.replaceUser(res.uid, {
+            uid: user.uid,
+            email: res.email,
+            access_level: res.access_level
+          }).then(user => this.addAuthenticated(user));
+        } else {
+          authorized = this.addAuthenticated(res);
+        }
       } else {
         this.addUnauthed(user);
       }
-      return res ? true : false;
+      return authorized;
     });
   }
 
-  static getUser(uid) {
-    // let root = this;
+  static getUser(uid, email) {
+    if (!uid && email) {
+      return firebase.database().ref('users')
+        .orderByChild('email').equalTo(email).once('value').then(snap => {
+          let user = {
+            uid,
+            email,
+            access_level: 0
+          };
+          let val = snap.val();
+          for (let key in val) {
+            user.uid = key;
+            user.email = val[key].email;
+            user.access_level = val[key].access_level;
+          }
+          console.log('user found', user);
+          return user;
+        });
+    }
     return firebase.database()
       .ref('users/' + uid)
       .once('value')
-      .then(snap => {
-        console.log('getting user', snap.val());
-        return snap.val();
+      .then(snap => snap.val());
+  }
+
+  static replaceUser(oldUid, user) {
+    return firebase.database().ref('users/' + oldUid).remove()
+      .then(() => {
+        return this.addUser(user);
+      })
+      .catch((error) => {
+        console.error("Remove failed: " + error.message);
+        return false;
       });
   }
 
