0a7e15c47f0e92aa8678d4f97b82be470b79cb17
Phil Schanely <philschanely@gmail.com>
2018-06-11 07:55:45 -0400

    Add user manager

--- src/components/ManageUsersPage.js
+++ src/components/ManageUsersPage.js
@@ -1,42 +1,69 @@
 import React, { Component } from 'react';
-import ArticleService from '../services/articles';
-import ArticleTable from './article/ArticleTable';
+import AuthService from '../services/auth';
+import UserService from '../services/user';
+
+import { Modal, ModalHeader, ModalTitle, ModalBody, ModalFooter } from "bs4-modal-react";
 
 class ManagerUsersPage extends Component {
 
+  accessLevels = [{
+      label: 'Authorized user',
+      value: 1
+    }, {
+      label: 'Administrator',
+      value: 2
+    }, {
+      label: 'Super Administrator',
+      value: 3
+  }];
+
+  currentUser = AuthService.getLocalUserData();
+
   state = {
-    articles: []
+    users: [],
+    showModal: false,
+    loading: true
   };
 
   constructor() {
     super();
-    this.deleteItem = this.deleteItem.bind(this);
+    this.doNotUpdate = false;
+    this.deleteUser = this.deleteUser.bind(this);
   }
 
   componentDidMount() {
-    this.getArticles();
+    this.getUsers();
   }
 
-  async getArticles() {
-    let articles = await ArticleService.getArticles();
+  shouldComponentUpdate() {
+    if (this.doNotUpdate) {
+      this.doNotUpdate = false;
+      return false;
+    }
+    return true;
+  }
+
+  async getUsers() {
+    let users = await UserService.getAll();
     this.setState({
-      articles
+      loading: false,
+      users
     });
   }
 
-  deleteItem(id) {
-    // On this page we want to physically remove a deleted recommendation
-    let articles = this.state.articles;
-    this.setState({
-      articles: []
-    });
+  deleteUser(e, uid) {
+    e.preventDefault();
+
+    // On this page we want to physically remove a deleted user
+    let users = this.state.users;
+
     let i = 0;
-    // So we search for the matching article as the one whose id is passed up to here
-    for (let article of articles) {
-      if (article.id === id) {
+    // So we search for the matching user as the one whose uid is passed up to here
+    for (let user of users) {
+      if (user.uid === uid) {
         // We delete it from Firebase and from this list
-        ArticleService.deleteArticle(id);
-        articles.splice(i, 1);
+        UserService.remove(uid);
+        users.splice(i, 1);
         break;
       }
       i = i + 1;
@@ -44,14 +71,142 @@ class ManagerUsersPage extends Component {
 
     // Modified list gets passed back to state
     this.setState({
-      articles
+      users
     })
+  }
+
+  handleChange = (e) => {
+    // e.preventDefault();
+    let activeUser = this.state.activeUser;
+    activeUser[e.target.name] = e.target.value;
+    this.doNotUpdate = true;
+    this.setState({ activeUser }, () => {
+      console.log('updated');
+    });
+  }
+
+  onHide = () => this.setState({ showModal: false });
+
+  onShow = () => this.setState({ showModal: true });
+
+  onSave = () => {
+    this.saveUser();
+    this.onHide();
+  };
+
+  async saveUser() {
+    console.log('saving user', this.state.activeUser);
+    await UserService.save(this.state.activeUser);
+    this.setState({
+      users: [],
+      loading: true
+    }, () => this.getUsers());
+  }
+
+  toggleEditUserModal(e, user) {
+    e.preventDefault();
+    if (!user) {
+      user =  {
+        email: '',
+        access_level: 1,
+        uuid: 0
+      }
+      console.log('adding user', user);
+    } else {
+      console.log('editing user', user);
+    }
+    this.setState({
+      activeUser: Object.assign({}, user),
+      showModal: true
+    });
+  }
+
+  userModal() {
+    let user = this.state.activeUser;
+    return (
+      <Modal visible={this.state.showModal} onHide={this.onHide}>
+        <ModalHeader>
+          <ModalTitle>Edit User</ModalTitle>
+        </ModalHeader>
+        <ModalBody>
+          {user ? (
+            <ul>
+              <li>
+                <label>Email</label>
+                <input type="email" name="email" defaultValue={user.email} onInput={this.handleChange} />
+              </li>
+              <li>
+                <label>Access level</label>
+                <select name="access_level" defaultValue={user.access_level} onChange={this.handleChange}>
+                  {this.accessLevels.map(al => {
+                    if (al.value > this.currentUser.access_level) {
+                      return false;
+                    }
+                    return (
+                      <option key={al.value} value={al.value}>{al.label}</option>
+                    );
+                  })}
+                </select>
+              </li>
+            </ul>
+          ) : (
+            <p>No user selected</p>
+          )}
+        </ModalBody>
+        <ModalFooter>
+          <button type="button" className="btn btn-secondary" onClick={this.onHide}>
+            Cancel
+          </button>
+          <button type="button" className="btn btn-primary" onClick={this.onSave}>
+            Save changes
+          </button>
+        </ModalFooter>
+      </Modal>
+    );
   }
 
   render() {
     return (
       <div className="ManagerUsersPage">
-        <p>Coming soon!</p>
+        <button className="btn btn-success float-right" onClick={(e) => this.toggleEditUserModal(e)}>Add User</button>
+        <h2 className="mt-3">Users </h2>
+        {this.state.loading ? (
+          <p>Loading users...</p>
+        ) : this.state.users ? (
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Email</th>
+                <th>Access Level</th>
+                <th>Options</th>
+              </tr>
+            </thead>
+            <tbody>
+            {this.state.users.map(user => {
+              let accessLevel = '';
+              for (let al of this.accessLevels) {
+                if (al.value === user.access_level) {
+                  accessLevel = al.label;
+                  break;
+                }
+              }
+              return (
+                <tr key={user.uid}>
+                  <td>{user.email}</td>
+                  <td>{accessLevel}</td>
+                  <td>
+                    <button className="btn btn-danger" onClick={(e) => this.deleteUser(e, user.uid)}>Remove</button>
+                    <button className="btn btn-warning" onClick={(e) => this.toggleEditUserModal(e, user)}>Edit</button>
+                  </td>
+                </tr>
+              );
+            })}
+            </tbody>
+          </table>
+        ): (
+          <p>No users found</p>
+        )}
+        {this.userModal()}
       </div>
     );
   }
--- src/services/user.js
+++ src/services/user.js
@@ -1,12 +1,13 @@
 import firebase from './firebase';
 
+import uuid from 'uuid/v4';
+
 export default class UserService {
 
   static add(data) {
-    console.log('adding user', data);
     return firebase.database().ref('users/' + data.uid).set({
         email: data.email,
-        access_level: data.access_level
+        access_level: Number(data.access_level)
       })
       .then(() => data)
       .catch((error) => {
@@ -41,13 +42,47 @@ export default class UserService {
       .then(snap => snap.val());
   }
 
+  static getAll() {
+    return firebase.database().ref('users/')
+      .orderByChild('email')
+      .once('value')
+      .then(snap => {
+        let users = [];
+        snap.forEach((dbUser) => {
+          users.push(Object.assign({ uid: dbUser.key }, dbUser.val()));
+        });
+        return users;
+      });
+  }
+
   static replace(oldUid, user) {
     return firebase.database().ref('users/' + oldUid)
       .remove()
-      .then(() => this.addUser(user))
+      .then(() => this.add(user))
       .catch((error) => {
         console.error("Remove failed: " + error.message);
         return false;
       });
+  }
+
+  static remove(uid) {
+    return firebase.database().ref('users/' + uid)
+      .remove()
+      .then(() => true)
+      .catch((error) => {
+        console.error("Remove failed: " + error.message);
+        return false;
+      });
+  }
+
+  static save(user) {
+    if (!user.uid) {
+      console.info('Adding new user', user);
+      user.uid = uuid();
+      return this.add(user);
+    } else {
+      console.info('saving existing user', user);
+      return this.add(user);
+    }
   }
 }
